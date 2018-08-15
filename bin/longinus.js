a63024e2f8c7b511deb4126b6dffef52edd96712
gkf442573575 <gkf442573575@163.com>
2018-08-15 14:25:27 +0800

    longinus init

--- bin/longinus.js
+++ bin/longinus.js
@@ -1,1 +1,23 @@
 #!/usr/bin/env node
+
+const program = require('commander');
+const packageJson = require('../package.json');
+
+
+program
+    .version(packageJson.version)
+    .usage('<command> [options]')
+
+program
+    .command('init <project-name>')
+    .description('Create a new project powered by longinus')
+    .action((name) => {
+        require('../lib/init')(name)
+    })
+
+program.on('--help', () => {
+    console.log('You can browse ' + packageJson.repository.url);
+
+})
+
+program.parse(process.argv)
